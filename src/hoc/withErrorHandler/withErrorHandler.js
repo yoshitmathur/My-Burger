7e43cef3acbba0745b17c715d596a2f8d2a8ff92
yoshitmathur <yoshitmathur@gmail.com>
2020-08-07 10:15:17 +0530

    Authentication

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,7 +9,7 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
             error: null
         }
 
-        componentWillMount () {
+        UNSAFE_componentWillMount () {
             this.reqInterceptor = axios.interceptors.request.use( req => {
                 this.setState( { error: null } );
                 return req;
@@ -40,7 +40,7 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
                 </Aux>
             );
         }
-    }
+    };
 }
 
 export default withErrorHandler;
---
+++ src/store/actions/auth.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId: userId
+    };
+};
+
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId')
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch=> {
+        setTimeout(()=>{
+            dispatch(logout())
+        }, expirationTime * 1000)
+    }
+}
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD-d8DJOOGpP555cpOdV18uFTgxYLCS6ZE';
+
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD-d8DJOOGpP555cpOdV18uFTgxYLCS6ZE';
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                const expirationDate = new Date( new Date().getTime() + response.data.expiresIn*1000 )
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err.response.data.error));
+            });
+    };
+};
+
+export const setAuthRedirectPath = (path)=> ({
+    type: actionTypes.SET_AUTH_REDIRECT_PATH,
+    path: path
+});
+
+export const authCheckState = () => {
+    return dispatch=> {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            const userId = localStorage.getItem('userId');
+            if(expirationDate > new Date()){
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(((expirationDate.getTime()-new Date().getTime()))/1000));
+            } else {
+                dispatch(logout());
+            }
+        }
+    }
+}
